14341ee084492e6c07ba7cb611c68b363328e8e7
jesse.picker@gmail.com <jesse.picker@gmail.com>
2021-04-29 17:52:19 -0400

    And, again.

--- Excemptions/Excemptions.cpp
+++ Excemptions/Excemptions.cpp
@@ -15,6 +15,7 @@ using namespace std;
 void character(char, int);
 void invalidCharacterExcpeption(char);
 void invalidRangeException(int);
+void excemptionForCharacter();
 
 int main() 
 {
@@ -22,6 +23,8 @@ int main()
 	int offset = 0;
 	
 	character(start, offset);
+
+	invalidCharacterExcpeption(start);
 
 
 
@@ -36,12 +39,23 @@ void character(char start, int offset)
 	cout << "\n\nEnter start key: ";
 		cin >> start;
 
-	cout << "\n\nEnter a number to offset (range is +- 12 from the middle M): ";
+	cout << "\n\nEnter a number to offset (the alphabet is comprised of 26 letters, if you choose the middle letter M, the range is +- 12 from the middle): ";
 	cin >> offset;
+
+
+	cout << "\nStart: " << start << "\t\t" << "Offset: " << offset << endl;
 }
 
-void invalidCharacterExcpeption(char)
+void invalidCharacterExcpeption(char start)
 {
+	try{ 
+		if (start == char)
+		{
+
+		}
+
+		
+	}
 
 
 }
