7943aa6f31715e78dd9878c7b2db2787cd1fd048
jesse.picker@gmail.com <jesse.picker@gmail.com>
2021-05-02 14:55:24 -0400

    updated

--- Excemptions/Excemptions.cpp
+++ Excemptions/Excemptions.cpp
@@ -13,24 +13,31 @@
 //Program Structure
 using namespace std;
 
+struct alphaOffset
+{
+	char start;
+	int alphaoffset;
+};
+
 //Function prototype
-char character(char);
-int offset(int);
+//char character(char);
+//int offset(int);
+alphaOffset returnValues(alphaOffset);
 
 int main() 
 {
 	char start = ' ';
 	int alphaoffset = 0;
-	char characterReceived;
-	int intReceived;
+	alphaOffset set;
+	alphaOffset setReturned;
 	
 	cout << "\n\nEnter start key: ";
-	cin >> start;
+	cin >> set.start;
 
-	cout << "\n\nEnter a number to offset (the alphabet is comprised of 26 letters, if you choose the middle letter M, the range is +- 12 from the middle): ";
-	cin >> alphaoffset;
+	cout << "\n\nEnter a number to offset (the alphabet is comprised of 26 letters, if you choose an offset which falls outside of what is allowed, I will throw an exception): ";
+	cin >> set.alphaoffset;
 
-	try
+	/*try
 	{
 		characterReceived = character(start);
 	}
@@ -46,44 +53,52 @@ int main()
 	catch (string characterExemption)
 	{
 		cout << characterExemption;
-	}
+	}*/
 
-	cout << "\n\nHere is what you entered:\n\n\nStart: " << start << "\t\t" << "Offset: " << alphaoffset << endl;
+	cout << "\n\nHere is what you entered:\n\n\nStart: " << set.start << "\t\t" << "Offset: " << set.alphaoffset << endl;
+
+	cout << "\n\nCalling the function: \n";
+	setReturned = returnValues(set);
+
 	cout << endl << endl;
 	system("pause");
 	return 0;
 }
 
-
-char character(char start)
+alphaOffset returnValues(alphaOffset)
 {
-	string characterExemption = "\nERROR: This is a thrown exemption for improper character input\n\nYou must enter a letter of the alphabet as a start key\n\n\nPROGRAM SELF DESTRUCTING....................AAAAAHHHHHHHHHHHHHHHH";
-	string exceptionString = "\nERROR: must enter a letter of the alphabet.";
-	
-		if (isalpha(start))
-		{
-			return start;
-		}
-		else
-		{
-			throw characterExemption;
-		}
+
 }
 
-int offset(int offset)
-{
-	int returnvalue;
-	string integerException = "\nERROR: This is a thrown exception for improper offset input\n\nYou must enter an offset number that doesn't extend beyond the maximum allowed by an alphabet comprised of 26 letters\n\n\nPROGRAM SELF DESTRUCTING....................AAAAAHHHHHHHHHHHHHHHH";
-	
 
-	if (offset < 26)
-	{
-		returnvalue = offset;
-		return returnvalue;
-	}
-	else
-	{
-		throw integerException;
-	}
-
-}
+//char character(char start)
+//{
+//	string characterExemption = "\nERROR: This is a thrown exemption for improper character input\n\nYou must enter a letter of the alphabet as a start key\n\n\nPROGRAM SELF DESTRUCTING....................AAAAAHHHHHHHHHHHHHHHH";
+//	string exceptionString = "\nERROR: must enter a letter of the alphabet.";
+//	
+//		if (isalpha(start))
+//		{
+//			return start;
+//		}
+//		else
+//		{
+//			throw characterExemption;
+//		}
+//}
+//
+//int offset(int offset)
+//{
+//	
+//	string integerException = "\nERROR: This is a thrown exception for improper offset input\n\nYou must enter an offset number that doesn't extend beyond the maximum allowed by an alphabet comprised of 26 letters\n\n\nPROGRAM SELF DESTRUCTING....................AAAAAHHHHHHHHHHHHHHHH";
+//	
+//
+//	if (offset < 26)
+//	{
+//		return offset;
+//	}
+//	else
+//	{
+//		throw integerException;
+//	}
+//
+//}
